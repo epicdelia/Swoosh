9948df2a7cad4d99c467560a0c7a8ff68b044ceb
epicdelia <epicdelia@gmail.com>
2019-11-08 16:17:03 -0500

    First two screens are done

---
+++ app/src/main/java/com/epicdelia/swoosh/LeagueActivity.kt
@@ -0,0 +1,12 @@
+package com.epicdelia.swoosh
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LeagueActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_league)
+    }
+}
--- app/src/main/java/com/epicdelia/swoosh/MainActivity.kt
+++ app/src/main/java/com/epicdelia/swoosh/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.epicdelia.swoosh
 
 import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        getstarted.setOnClickListener{
+            val leagueIntent = Intent(this, LeagueActivity::class.java)
+            startActivity(leagueIntent);
+        }
     }
 }
